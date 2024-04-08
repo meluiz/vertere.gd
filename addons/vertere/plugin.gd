6c7db61d9ba9091ccc28ba92bdc00edfd98bdca3
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2024-04-07 21:42:32 -0300

    fix: remove `Vertere` and `Utils` from autoload on _exit_tree

--- addons/vertere/plugin.gd
+++ addons/vertere/plugin.gd
@@ -4,3 +4,7 @@ extends EditorPlugin
 func _enter_tree():
 	add_autoload_singleton("Utils", "res://addons/Vertere/utils.gd")
 	add_autoload_singleton("Vertere", "res://addons/Vertere/vertere.gd")
+
+func _exit_tree():
+	remove_autoload_singleton("Utils")
+	remove_autoload_singleton("Vertere")
